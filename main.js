346c04c16ea5e9aefa27432df6646ccbc182eeb9
NicholasC2 <nicholasc2@proton.me>
2026-05-27 23:19:10 +1200

    final

--- main.js
+++ main.js
@@ -1,14 +1,190 @@
-const { BrowserWindow, app, ipcMain, Menu } = require("electron");
+const { BrowserWindow, app, ipcMain, dialog, screen } = require("electron");
 const path = require("path");
+const fs = require("fs");
+const unzipper = require("unzipper");
 
-let win;
-let win2;
+let menuWindow = null;
+let mainWindow = null;
 
-function createWindow() {
+let petAnimations = [];
+let petManifest = null;
 
-	win = new BrowserWindow({
-		width: 220,
-		height: 256,
+let animationSpeed = 60;
+let reflect = 0.3;
+
+function safeCloseMenu() {
+	if (!menuWindow || menuWindow.isDestroyed()) return;
+
+	menuWindow.removeAllListeners("blur");
+	menuWindow.close();
+	menuWindow = null;
+}
+
+function openMenu(x, y, html = "") {
+	if (menuWindow && !menuWindow.isDestroyed()) {
+		safeCloseMenu()
+	}
+
+	menuWindow = new BrowserWindow({
+		width: 200,
+		height: 1,
+		x,
+		y,
+		transparent: true,
+		frame: false,
+		alwaysOnTop: true,
+		resizable: false,
+		focusable: true,
+		webPreferences: {
+			nodeIntegration: true,
+			contextIsolation: false
+		}
+	});
+
+	menuWindow.loadFile(path.join(__dirname, "menu.html"));
+
+	menuWindow.webContents.on("did-finish-load", () => {
+		menuWindow.webContents.send("menu-content", html);
+	});
+
+	menuWindow.on("blur", () => {
+		if (menuWindow && !menuWindow.isDestroyed()) {
+			safeCloseMenu();
+		}
+		menuWindow = null;
+	});
+}
+
+ipcMain.on("menu-height", (event, height) => {
+	if (!menuWindow || menuWindow.isDestroyed()) return;
+
+	const bounds = menuWindow.getBounds();
+	const display = screen.getDisplayNearestPoint(bounds);
+	const work = display.workArea;
+
+	let x = bounds.x;
+	let y = bounds.y;
+	const width = bounds.width;
+
+	if (x + width > work.x + work.width) {
+		x = x - width;
+	}
+
+	x = Math.max(work.x, x);
+
+	if (y + height > work.y + work.height) {
+		y = work.y + work.height - height;
+	}
+
+	y = Math.max(work.y, y);
+
+	menuWindow.setBounds({
+		x,
+		y,
+		height: Math.ceil(height)
+	});
+});
+
+ipcMain.on("click", async(e,i) => {
+	if(i.elem == "menu" || i.elem == "row" || i.elem == "animation-speed-slider") return;
+
+	if (menuWindow && !menuWindow.isDestroyed()) {
+		safeCloseMenu()
+	}
+	menuWindow = null;
+
+	if(i.elem == "set-pet") {
+		const result = await dialog.showOpenDialog(mainWindow, {
+			title: "Select a pet",
+			properties: ["openFile"],
+			filters: [
+				{extensions: ["pet"], name: "Pet File"},
+				{extensions: ["zip"], name: "Zip Archive"},
+    			{ name: "All Files", extensions: ["*"] }
+			]
+		})
+
+		if(!result.canceled) {
+			loadPet(result.filePaths[0]);
+		}
+	}
+
+	if(i.elem == "quit") {
+		process.exit(0)
+	}
+});
+
+async function loadPet(path) {
+	const [x, y] = mainWindow.getPosition();
+
+	const directory = await unzipper.Open.file(path);
+
+	petAnimations = [];
+	petManifest = null;
+
+	const imageMap = {};
+
+	const tasks = directory.files.map(async (file) => {
+
+		if (file.path.endsWith("manifest.json")) {
+			try {
+				const parsedManifest = JSON.parse(
+					(await file.buffer()).toString("utf8")
+				);
+
+				if (Array.isArray(parsedManifest.animations)) {
+					petManifest = parsedManifest;
+					openMenu(x, y, "Pet Loaded!");
+				}
+			} catch (err) {
+				openMenu(x, y, "Invalid Manifest!");
+			}
+			return;
+		}
+
+		const ext = file.path.split(".").at(-1).toLowerCase();
+
+		if (["png", "jpg", "jpeg"].includes(ext)) {
+			const mime = ext === "jpg" ? "jpeg" : ext;
+
+			const buffer = await file.buffer();
+
+			imageMap[file.path] =
+				`data:image/${mime};base64,${buffer.toString("base64")}`;
+		}
+	});
+
+	await Promise.all(tasks);
+
+	if (petManifest && Array.isArray(petManifest.animations)) {
+
+		petAnimations = petManifest.animations.map(anim => ({
+			name: anim.name,
+			url: imageMap[anim.path] || null,
+			width: anim.width
+		}));
+	}
+}
+
+app.whenReady().then(()=>{
+	let reflectionWindow = new BrowserWindow({
+		width: 200,
+		height: 200,
+		transparent: true,
+		frame: false,
+		alwaysOnTop: true,
+		resizable: false,
+		focusable: false,
+
+		webPreferences: {
+			nodeIntegration: true,
+			contextIsolation: false
+		}
+	});
+
+	mainWindow = new BrowserWindow({
+		width: 200,
+		height: 200,
 		transparent: true,
 		frame: false,
 		alwaysOnTop: true,
@@ -21,152 +197,104 @@ function createWindow() {
 		}
 	});
 
-	win2 = new BrowserWindow({
-		width: 220,
-		height: 256,
-		transparent: true,
-		frame: false,
-		alwaysOnTop: true,
-		resizable: false,
-		focusable: false,
+	mainWindow.setAlwaysOnTop(true, "screen-saver");
+	reflectionWindow.setAlwaysOnTop(true, "screen-saver");
 
-		webPreferences: {
-			nodeIntegration: true,
-			contextIsolation: false
-		}
+	mainWindow.setVisibleOnAllWorkspaces(true, { visibleOnFullScreen: true });
+	reflectionWindow.setVisibleOnAllWorkspaces(true, { visibleOnFullScreen: true });
+
+	function setReflectionPos() {
+		const [x, y] = mainWindow.getPosition();
+		const [w, h] = mainWindow.getSize();
+
+		reflectionWindow.setPosition(x, y + h - (petManifest?.reflectionOffset ?? 0));
+	}
+
+	setReflectionPos()
+
+	mainWindow.loadFile(path.join(__dirname, "index.html"));
+
+	reflectionWindow.loadFile(path.join(__dirname, "reflection.html"))
+
+	mainWindow.on("close", ()=>{
+		process.exit(0);
 	})
 
-	win2.setAlwaysOnTop(true, "screen-saver");
-	win2.setVisibleOnAllWorkspaces(true, { visibleOnFullScreen: true })
-	win2.setIgnoreMouseEvents(true, {
-		forward: true
+	ipcMain.on("set-size", (e, width, height) => {
+		reflectionWindow.setSize(width, height);
+		mainWindow.setSize(width, height);
+	});
+
+	mainWindow.on("move", setReflectionPos)
+	mainWindow.on("resize", setReflectionPos)
+
+	ipcMain.on("set-animation", (event, animation, width) => {
+		mainWindow.webContents.send("set-animation", animation, width)
+		reflectionWindow.webContents.send("set-animation", animation, width)
+	})
+	
+	ipcMain.on("set-animation-speed", (e, speed) => {
+		animationSpeed = speed;
+
+		mainWindow.webContents.send("set-animation-speed", animationSpeed)
 	})
 
-	win2.loadFile(path.join(__dirname, "reflection.html"));
+	ipcMain.on("set-animation-pos", (e, pos) => {
+		reflectionWindow.send("set-animation-pos", pos)
+	})
 
-	const syncReflectionPosition = () => {
-		const [x, y] = win.getPosition()
-		const [w, h] = win.getSize()
+	ipcMain.on("set-animation-reflection", (e,reflection) => {
+		reflect = reflection;
 
-		win2.setBounds({
-			x,
-			y: y + 242,
-			width: w,
-			height: 256
-		})
-	}
+		reflectionWindow.webContents.send("set-animation-reflection", reflect)
+	})
 
-	syncReflectionPosition()
-
-	win.on('move', syncReflectionPosition)
-
-  	win.setAlwaysOnTop(true, "screen-saver");
-  	win.setVisibleOnAllWorkspaces(true, { visibleOnFullScreen: true });
-
-	win.loadFile(path.join(__dirname, "index.html"));
-
-	win.webContents.on("before-input-event", (event, input) => {
-	const key = input.key?.toLowerCase();
-
-	if (
-		(input.control && (key === "=" || key === "-")) ||
-		(input.meta && (key === "=" || key === "-"))
-	) {
-		event.preventDefault();
-	}
+	ipcMain.on("open-menu", (event, { x, y }) => {
+		openMenu(x, y, `
+			<button class='set-pet'>Set Pet</button>
+			${petAnimations.length == 0 ? "" : `
+				<hr>
+				${petAnimations.map(anim => `
+					<button onclick='setAnimation("${anim.url}", ${anim.width})'>
+						${anim.name}
+					</button>
+				`).join("")}
+				<hr>
+				Animation Speed
+				<div class="row">
+					<input class="animation-speed-slider" type="range" min="20" max="300" step="1" value="${animationSpeed}" oninput="setAnimationSpeed(this.value)">
+					<span class="animation-speed-label">${animationSpeed}BPM</span>
+				</div>
+				<hr>
+				Reflect
+				<div class="row">
+					<input class="reflect-slider" type="range" min="0" max="1" step="0.01" value="${reflect}" oninput="setReflect(this.value)">
+					<span class="reflect-label">${reflect}</span>
+				</div>
+			`}
+			<hr>
+			<button class='quit'>Quit</button>
+		`);
 	});
 
-	function close() {
-		process.exit();
-	}
+	let isDragging = false;
 
-	win.on("close", close)
-	win2.on("close", close)
-}
+	ipcMain.on("drag-start", () => {
+		isDragging = true;
+	});
 
-let dragging = false;
-let offset = { x: 0, y: 0 };
+	ipcMain.on("drag-move", (e, data) => {
+		if (!isDragging) return;
 
-ipcMain.on("drag-start", (event, mousePos) => {
+		const [winX, winY] = mainWindow.getSize();
 
-	win2.webContents.send("drag-start")
+		const newX = (data.x);
+		const newY = (data.y);
 
-	dragging = true;
+		mainWindow.setPosition(newX - winX/2, newY - winY/2);
+	});
 
-	const [winX, winY] = win.getPosition();
-
-	offset = {
-		x: mousePos.x - winX,
-		y: mousePos.y - winY
-	};
-});
-
-ipcMain.on("drag-move", (event, mousePos) => {
-
-	if (!dragging) return;
-
-	win.setPosition(
-		mousePos.x - offset.x,
-		mousePos.y - offset.y
-	);
-});
-
-ipcMain.on("drag-end", () => {
-	win2.webContents.send("drag-end")
-
-	dragging = false;
-});
-
-ipcMain.on("reflect-state", (e, i) => {
-	win2.webContents.send("reflect-state", i);
-})
-
-const animations = [
-	"Waiting",
-	"Stepping",
-	"Jumping",
-	"Zombie",
-	"Waving",
-	"Hula",
-	"Windmill",
-	"Zitabata",
-	"Dervish"
-];
-
-let currentAnimation = 0;
-
-ipcMain.on("show-context-menu", () => {
-
-	const template = animations.map((name, i) => ({
-		label: name,
-		type: "radio",
-		checked: i === currentAnimation,
-		click: () => {
-			currentAnimation = i;
-			win.webContents.send("set-animation", i);
-		}
-	}));
-
-	template.push(
-		{ type: "separator" },
-		{
-			label: "Show/Hide Options",
-			click: () => {
-				win.webContents.send("toggle-options");
-			}
-		},
-		{ type: "separator" },
-		{
-			label: "Quit",
-			role: "quit"
-		}
-	);
-
-	const menu = Menu.buildFromTemplate(template);
-
-	menu.popup({
-		window: win
+	ipcMain.on("drag-end", () => {
+		isDragging = false;
 	});
 });
-
-app.whenReady().then(createWindow);
